307c6c036da2e048f6abb515b7eb13a53b3cc648
julienwyss <julien.wyss99@gmail.com>
2025-04-01 14:39:00 +0200

    Adding BootstrapSceneManager.cs + Meta

---
+++ FortressForge/Assets/Scripts/Network/BootstrapSceneManager.cs
@@ -0,0 +1,41 @@
+﻿using FishNet;
+using FishNet.Managing.Scened;
+using UnityEngine;
+
+namespace FortressForge.Network
+{
+    /// <summary>
+    /// Manages the loading and unloading of scenes for all clients
+    /// </summary>
+    public class BootstrapSceneManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+        }
+
+        /// <summary>
+        /// Loads a scene for all clients
+        /// </summary>
+        /// <param name="sceneName">The name of the scene to load</param>
+        public void LoadScene(string sceneName)
+        {
+            if (!InstanceFinder.IsServerStarted) return;
+            
+            SceneLoadData sceneLoadData = new (sceneName);
+            InstanceFinder.SceneManager.LoadGlobalScenes(sceneLoadData);
+        }
+
+        /// <summary>
+        /// Unloads a scene for all clients
+        /// </summary>
+        /// <param name="sceneName">The name of the scene to unload</param>
+        public void UnloadScene(string sceneName)
+        {
+            if (!InstanceFinder.IsServerStarted) return;
+            
+            SceneUnloadData sceneUnloadData = new (sceneName);
+            InstanceFinder.SceneManager.UnloadGlobalScenes(sceneUnloadData);
+        }
+    }
+}
