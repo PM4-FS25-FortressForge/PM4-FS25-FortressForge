a1722437d35efee1d5dac2a9a8de2695a74cdbde
Fabio Geisser <fabio.geisser@gmail.com>
2025-04-03 00:54:55 +0200

    Test :)

--- FortressForge/Assets/Tests/Hexgrid/HexgridTest.cs
+++ FortressForge/Assets/Tests/Hexgrid/HexgridTest.cs
@@ -10,75 +10,67 @@ namespace Tests.Hexgrid
     public class HexGridDataTests
     {
         [Test]
-        [TestCase(0, 0, 1)]
-        [TestCase(0, 0, 0)]
-        [TestCase(99999, 0, 0)]
-        [TestCase(5, 5, 0)]
-        public void TestValidateBuildingPlacement_Returns_False(int x, int y, int z)
+        public void ValidateBuildingPlacement_ReturnsTrue_IfAllTilesAreFree()
         {
+            
+            // Arrange
             HexGridData gridData = new HexGridData(
                 id: 1,
                 origin: Vector3.zero,
-                radius: 5,
-                maxBuildHight: 5,
+                radius: 1,
+                height: 1,
                 tileSize: 1f,
                 tileHeight: 2f
             );
-            
-            HexTileCoordinate alreadyOccupiedCoord = new HexTileCoordinate(0, 0, 0);
-            gridData.TileMap[alreadyOccupiedCoord].IsOccupied = true;
-            
-            BaseBuildingTemplate buildingTemplate = ScriptableObject.CreateInstance<WeaponBuildingTemplate>();
+
+            // Beispiel-BuildingTemplate, das nur EIN Tile belegt
+            BaseBuildingTemplate buildingTemplate = ScriptableObject.CreateInstance<BaseBuildingTemplate>();
             buildingTemplate.ShapeData = new List<HexTileCoordinate>{
-                new HexTileCoordinate(0, 0, 0),
-                new HexTileCoordinate(1, 0, 0),
-                new HexTileCoordinate(0, 1, 0),
-                new HexTileCoordinate(1, 1, 0),
+                new HexTileCoordinate(0, 0, 0) // belegt nur den "Mittelpunkt"
             };
-            
-            HexTileCoordinate placementCoord = new HexTileCoordinate(x, y, z);
+
+            HexTileCoordinate placementCoord = new HexTileCoordinate(0, 0, 0);
 
             // Act
             bool canPlace = gridData.ValidateBuildingPlacement(placementCoord, buildingTemplate);
 
             // Assert
-            Assert.IsFalse(canPlace,
-                $"ValidateBuildingPlacement sollte false zurückgeben, wenn Tile ({x},{y},{z}) bereits belegt ist.");
+            Assert.IsTrue(canPlace, "Der Gebäude-Bau sollte erlaubt sein, wenn alle Tiles frei sind.");
+
+            // Zusätzlich können wir überprüfen, ob das Tile jetzt als belegt (IsOccupied=true) markiert wurde
+            var placedTile = gridData.TileMap[placementCoord];
+            Assert.IsTrue(placedTile.IsOccupied,
+                "Das Tile sollte nach erfolgreichem Platzieren als belegt markiert sein.");
         }
-        
+
         [Test]
-        [TestCase(2, 2, 0)]
-        [TestCase(3, -3, 0)]
-        public void TestValidateBuildingPlacement_Returns_True(int x, int y, int z)
+        public void TestValidateBuildingPlacement_Returns_False()
         {
             HexGridData gridData = new HexGridData(
                 id: 1,
                 origin: Vector3.zero,
                 radius: 5,
-                maxBuildHight: 5,
+                height: 2,
                 tileSize: 1f,
                 tileHeight: 2f
             );
-            
-            HexTileCoordinate alreadyOccupiedCoord = new HexTileCoordinate(0, 0, 0);
+
+            HexTileCoordinate alreadyOccupiedCoord = new HexTileCoordinate(0, 1, 0);
             gridData.TileMap[alreadyOccupiedCoord].IsOccupied = true;
-            
+
             BaseBuildingTemplate buildingTemplate = ScriptableObject.CreateInstance<WeaponBuildingTemplate>();
             buildingTemplate.ShapeData = new List<HexTileCoordinate>{
                 new HexTileCoordinate(0, 0, 0),
-                new HexTileCoordinate(1, 0, 0),
-                new HexTileCoordinate(0, 1, 0),
-                new HexTileCoordinate(1, 1, 0),
+                new HexTileCoordinate(1, -1, 0),
+                new HexTileCoordinate(0, 0, 1)
             };
+
+            HexTileCoordinate placementCoord = new HexTileCoordinate(0, 0, 0);
             
-            HexTileCoordinate placementCoord = new HexTileCoordinate(x, y, z);
-
-            // Act
             bool canPlace = gridData.ValidateBuildingPlacement(placementCoord, buildingTemplate);
-
-            // Assert
-            Assert.IsTrue(canPlace,
-                $"ValidateBuildingPlacement sollte true zurückgeben, wenn Tile ({x},{y},{z}) bereits belegt ist.");
+            
+            Assert.IsFalse(canPlace,
+                "ValidateBuildingPlacement sollte false zurückgeben, wenn eines der Tiles bereits belegt ist.");
         }
     }
 }
