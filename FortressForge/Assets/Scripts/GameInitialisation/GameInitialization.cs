d1533a713e1dd82aa9429cb61f961c5105a5b68a
kryezleo <kryezleo@students.zhaw.ch>
2025-04-06 14:07:38 +0200

    Added functionality to allow the host of a lobby to set available buildings and technologies, and select the game map. Updated GameInitializer class to include methods for setting these options and modified the InitializeGame method to accept additional parameters for buildings, technologies, and map selection

--- FortressForge/Assets/Scripts/GameInitialisation/GameInitialization.cs
+++ FortressForge/Assets/Scripts/GameInitialisation/GameInitialization.cs
@@ -14,8 +14,16 @@ namespace FortressForge.GameInitialization
         /// Initializes the game for the given teams.
         /// </summary>
         /// <param name="teams">List of teams to initialize the game for.</param>
-        public void InitializeGame(List<Team> teams)
+        /// <param name="availableBuildings">List of available buildings.</param>
+        /// <param name="availableTechnologies">List of available technologies.</param>
+        /// <param name="selectedMap">The selected game map.</param>
+        public void InitializeGame(List<Team> teams, List<string> availableBuildings, List<string> availableTechnologies, string selectedMap)
         {
+            // Set available buildings and technologies
+            SetAvailableBuildings(availableBuildings);
+            SetAvailableTechnologies(availableTechnologies);
+            SelectGameMap(selectedMap);
+
             foreach (var team in teams)
             {
                 foreach (var player in team.Players)
@@ -31,13 +39,40 @@ namespace FortressForge.GameInitialization
         /// <param name="player">The player to initialize hex grids for.</param>
         private void InitializeHexGridsForPlayer(Player player)
         {
-            player.HexGrids = new List<HexGridData> { new HexGridData(0, new Vector3(), 0, 0, 0, 0) }; // Pass required parameters
+            player.HexGrids = new List<HexGridData> { new HexGridData(0, new Vector3(), 0, 0, 0, 0) };
 
             foreach (var hexGrid in player.HexGrids)
             {
                 var hexGridHoverController = new GameObject("HexGridHoverController").AddComponent<HexGridHoverController>();
                 hexGridHoverController.CurrentlyHoveredTile = null;
             }
+        }
+
+        /// <summary>
+        /// Sets the available buildings for the game.
+        /// </summary>
+        /// <param name="availableBuildings">List of available buildings.</param>
+        private void SetAvailableBuildings(List<string> availableBuildings)
+        {
+            // Implementation to set available buildings
+        }
+
+        /// <summary>
+        /// Sets the available technologies for the game.
+        /// </summary>
+        /// <param name="availableTechnologies">List of available technologies.</param>
+        private void SetAvailableTechnologies(List<string> availableTechnologies)
+        {
+            // Implementation to set available technologies
+        }
+
+        /// <summary>
+        /// Selects the game map.
+        /// </summary>
+        /// <param name="selectedMap">The selected game map.</param>
+        private void SelectGameMap(string selectedMap)
+        {
+            // Implementation to select the game map
         }
     }
 }
