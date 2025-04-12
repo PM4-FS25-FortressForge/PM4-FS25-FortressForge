88810a045a5f2b54b190b17b4fdc0dcc96e10ba8
Patrick Zürcher <70685898+Plantoloz@users.noreply.github.com>
2025-04-12 19:07:17 +0200

    55 combine economy with buildings (#73)
    * Implemented basic economy
    
    * Added tests and cost
    
    * removed a todo
    
    * Adjusted with comments
    
    * Removed unused code and comments in BuildViewController
    
    * Replaced Wood resource with Magma in economy system tests and updated related comments
    Carefull, your house might now be on fire
    
    * Implemented basic economy
    
    * Added tests and cost
    
    * removed a todo
    
    * Adjusted with comments
    
    * Removed unused code and comments in BuildViewController
    
    * Replaced Wood resource with Magma in economy system tests and updated related comments
    Carefull, your house might now be on fire
    
    * Remove unnecessary header for resource data in BaseBuildingTemplate

--- FortressForge/Assets/Scripts/BuildingSystem/HexGrid/HexGridData.cs
+++ FortressForge/Assets/Scripts/BuildingSystem/HexGrid/HexGridData.cs
@@ -68,7 +68,7 @@ namespace FortressForge.BuildingSystem.HexGrid
         /// Validates a tile if its already occupied, then if not Marks it as occupied and creates the tile above if needed.
         /// </summary>
         public bool ValidateBuildingPlacement(HexTileCoordinate hexCoord, BaseBuildingTemplate buildingTemplate)
-        {
+        { // TODO: THis method does more then just validate the placement, consider renaming, or better splitting it up
             foreach (var coord in buildingTemplate.ShapeData)
             {
                 TileMap.TryGetValue((hexCoord + coord), out var tileData);
@@ -77,7 +77,7 @@ namespace FortressForge.BuildingSystem.HexGrid
                     Debug.Log("Placement failed");
                     return false;
                 }
-                   
+
                 UpdateHexTileData(hexCoord);
             }
             
---
+++ FortressForge/Assets/Tests/EconomyManager/ResourceTests.cs
@@ -0,0 +1,99 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using FortressForge.BuildingSystem.BuildingData;
+using FortressForge.Economy;
+using FortressForge.BuildingSystem.BuildManager;
+
+public class EconomySystem_PayResourceIfSufficientTests
+{
+    private EconomySystem _economySystem;
+
+    [SetUp]
+    public void SetUp()
+    {
+        var buildingManager = new BuildingManager(); // empty, not needed here
+        var maxValues = new Dictionary<ResourceType, float>
+        {
+            { ResourceType.Metal, 100 },
+            { ResourceType.Power, 100 },
+            { ResourceType.Magma, 100 }
+        };
+
+        _economySystem = new EconomySystem(buildingManager, maxValues);
+
+        // Set initial amounts manually for full control
+        _economySystem.CurrentResources[ResourceType.Metal].CurrentAmount = 80;
+        _economySystem.CurrentResources[ResourceType.Power].CurrentAmount = 50;
+        _economySystem.CurrentResources[ResourceType.Magma].CurrentAmount = 0;
+    }
+
+    [Test]
+    public void PayResourceIfSufficient_SufficientResources_ReturnsTrueAndDeducts()
+    {
+        var cost = new Dictionary<ResourceType, float>
+        {
+            { ResourceType.Metal, 30 },
+            { ResourceType.Power, 20 }
+        };
+
+        var result = _economySystem.PayResourceIfSufficient(cost);
+
+        Assert.IsTrue(result);
+        Assert.AreEqual(50, _economySystem.CurrentResources[ResourceType.Metal].CurrentAmount);
+        Assert.AreEqual(30, _economySystem.CurrentResources[ResourceType.Power].CurrentAmount);
+    }
+
+    [Test]
+    public void PayResourceIfSufficient_ExactAmount_ReturnsTrueAndZeroesOut()
+    {
+        var cost = new Dictionary<ResourceType, float>
+        {
+            { ResourceType.Power, 50 }
+        };
+
+        var result = _economySystem.PayResourceIfSufficient(cost);
+
+        Assert.IsTrue(result);
+        Assert.AreEqual(0, _economySystem.CurrentResources[ResourceType.Power].CurrentAmount);
+    }
+
+    [Test]
+    public void PayResourceIfSufficient_InsufficientResources_ReturnsFalseAndNoChange()
+    {
+        var cost = new Dictionary<ResourceType, float>
+        {
+            { ResourceType.Metal, 200 }
+        };
+
+        var result = _economySystem.PayResourceIfSufficient(cost);
+
+        Assert.IsFalse(result);
+        Assert.AreEqual(80, _economySystem.CurrentResources[ResourceType.Metal].CurrentAmount);
+    }
+
+    [Test]
+    public void PayResourceIfSufficient_OneResourceInsufficient_AbortsAndPreservesAll()
+    {
+        var cost = new Dictionary<ResourceType, float>
+        {
+            { ResourceType.Metal, 50 },
+            { ResourceType.Magma, 10 } // Not enough
+        };
+
+        var result = _economySystem.PayResourceIfSufficient(cost);
+
+        Assert.IsFalse(result);
+        Assert.AreEqual(80, _economySystem.CurrentResources[ResourceType.Metal].CurrentAmount);
+        Assert.AreEqual(0, _economySystem.CurrentResources[ResourceType.Magma].CurrentAmount);
+    }
+
+    [Test]
+    public void PayResourceIfSufficient_EmptyCost_ReturnsTrue()
+    {
+        var result = _economySystem.PayResourceIfSufficient(new Dictionary<ResourceType, float>());
+
+        Assert.IsTrue(result);
+        Assert.AreEqual(80, _economySystem.CurrentResources[ResourceType.Metal].CurrentAmount);
+        Assert.AreEqual(50, _economySystem.CurrentResources[ResourceType.Power].CurrentAmount);
+    }
+}
