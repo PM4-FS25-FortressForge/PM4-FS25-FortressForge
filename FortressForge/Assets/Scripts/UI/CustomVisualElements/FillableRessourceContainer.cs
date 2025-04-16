7de27204e317fe86e8d3cf36aa41a060467617de
julienwyss <julien.wyss99@gmail.com>
2025-04-16 16:06:00 +0200

    adding comments

--- FortressForge/Assets/Scripts/UI/CustomVisualElements/FillableRessourceContainer.cs
+++ FortressForge/Assets/Scripts/UI/CustomVisualElements/FillableRessourceContainer.cs
@@ -3,6 +3,9 @@
     using UnityEngine;
     using UnityEngine.UIElements;
 
+    /// <summary>
+    /// A custom visual element that represents a fillable resource container.
+    /// </summary>
     [UxmlElement("FillableRessourceContainer")]
     public partial class FillableRessourceContainer : VisualElement
     {
@@ -10,6 +13,9 @@
         private float _fillPercentage;
         private bool _isHorizontal;
 
+        /// <summary>
+        /// Gets or sets the fill percentage of the container.
+        /// </summary>
         public float FillPercentage
         {
             get => _fillPercentage;
@@ -27,6 +33,9 @@
             }
         }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether the container is horizontal.
+        /// </summary>
         public bool IsHorizontal
         {
             get => _isHorizontal;
@@ -37,6 +46,9 @@
             }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FillableRessourceContainer"/> class.
+        /// </summary>
         public FillableRessourceContainer()
         {
             _fillElement = new VisualElement();
@@ -45,6 +57,9 @@
             Add(_fillElement);
         }
 
+        /// <summary>
+        /// Updates the orientation of the container based on the current setting.
+        /// </summary>
         private void UpdateOrientation()
         {
             if (_isHorizontal)
@@ -61,6 +76,10 @@
             }
         }
 
+        /// <summary>
+        /// Adds a style class to the container's class list.
+        /// </summary>
+        /// <param name="className">The name of the class to add.</param>
         public void AddStyleForClassList(string className)
         {
             if (!string.IsNullOrEmpty(className))
