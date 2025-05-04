4eed05063899af9261e06e64bab5f43bf27c2b4c
julienwyss <julien.wyss99@gmail.com>
2025-05-04 16:56:03 +0200

    Refactor WaitForInitialization method: streamline initialization checks and add timeout logging for HexGridManager readiness

--- FortressForge/Assets/Scripts/GameInitialization/PlayerInitializationManager.cs
+++ FortressForge/Assets/Scripts/GameInitialization/PlayerInitializationManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using FishNet;
@@ -20,36 +21,37 @@ namespace FortressForge.GameInitialization
 {
     public class PlayerInitializationManager : NetworkBehaviour
     {
-        [Header("Game Start Configuration")]
-        [SerializeField] private GameStartConfiguration _gameStartConfiguration;
+        [Header("Game Start Configuration")] [SerializeField]
+        private GameStartConfiguration _gameStartConfiguration;
+
         [SerializeField] private GameSessionStartConfiguration _gameSessionStartConfiguration;
-        
+
         public override void OnStartClient()
         {
             // Initialize Client for each player so ObserverRpc can be used
             StartCoroutine(WaitForInitialization());
         }
-        
+
         public override void OnStartServer()
         {
             // Initialize Server for each player so ObserverRpc can be used. This gets called on each joining client on server side.
             // This is a bit of a hack, for optimization but higher complexity you could differentiate between server and client more.
             Init();
         }
-        
+
         /// <summary>
         /// Waits for the initialization of the HexGridManager and the AllGrids list.
         /// </summary>
         /// <returns>IEnumerator</returns>
         private IEnumerator WaitForInitialization()
         {
-            while (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "LobbyScene" || 
-                   HexGridManager.Instance is null || 
-                   HexGridManager.Instance.AllGrids is null || 
-                   HexGridManager.Instance.AllGrids.Count == 0)
-            {
-                yield return null;
-            }
+            yield return new WaitUntil(() =>
+                    UnityEngine.SceneManagement.SceneManager.GetActiveScene().name != "LobbyScene" &&
+                    HexGridManager.Instance != null &&
+                    HexGridManager.Instance.AllGrids != null &&
+                    HexGridManager.Instance.AllGrids.Count > 0
+                , new TimeSpan(0, 0, 10),
+                () => Debug.Log("HexGridManager is not initialized after 10 seconds."));
             Init();
         }
 
@@ -66,42 +68,42 @@ namespace FortressForge.GameInitialization
 
             // Currently takes the playerId from the owner of this object
             int playerId = Owner.ClientId;
-            
+
             // Take the clientId from the owner of this object, also called the playerId
             int gridId = _gameSessionStartConfiguration.GridPlayerIdTuples
                 .First(gpit => gpit.PlayerId == playerId).HexGridId;
             HexGridData selectedGrid = HexGridManager.Instance.AllGrids[gridId];
-            
+
             // initialize the grid view so allgrids is set
             BuildViewController buildViewController = gameObject.GetComponent<BuildViewController>();
             // We only initialize the view for the selected grid,
             // theoretically you could add multiple grids per player here. But EconomySystem is only one per player. So there mustn't be overlaps.
-            buildViewController.Init(new List<HexGridData>{ selectedGrid }, 
+            buildViewController.Init(new List<HexGridData> { selectedGrid },
                 _gameStartConfiguration, HexGridManager.Instance);
-            
+
             // After creating EconomySystem
             var economySync = gameObject.GetComponent<EconomySync>();
             EconomyController economyController = gameObject.AddComponent<EconomyController>();
-            
+
             // Only the server needs to initialize the EconomySystem
             if (IsServerInitialized && !IsClientInitialized)
             {
                 economySync.Init(selectedGrid.EconomySystem);
                 economyController.Init(selectedGrid.EconomySystem);
             }
-            
+
             // Initialize view only on clients, server doesn't need the individual views
             if (IsClientInitialized && IsOwner)
             {
                 Vector3 gridOrigin = _gameSessionStartConfiguration.HexGridOrigins[gridId];
-                
+
                 Camera mainCamera = Camera.main;
                 if (mainCamera == null)
                 {
                     Debug.LogError("Main camera not found!");
                     return;
                 }
-                
+
                 mainCamera.GetComponent<CameraController>()
                     .SetTargetPosition(gridOrigin);
 
