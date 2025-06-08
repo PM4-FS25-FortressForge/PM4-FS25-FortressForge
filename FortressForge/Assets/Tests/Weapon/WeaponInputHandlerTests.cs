ab3994ce6c6cb68974816609bf58b7d2c0d01fec
esposdie <esposdie@zhaw.students.ch>
2025-06-08 21:49:05 +0200

    Update

--- FortressForge/Assets/Tests/Weapon/WeaponInputHandlerTests.cs
+++ FortressForge/Assets/Tests/Weapon/WeaponInputHandlerTests.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections;
 using System.Reflection;
-using System.Text.RegularExpressions;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 using FishNet;
-using FishNet.Object;
 using Object = UnityEngine.Object;
 using FortressForge.BuildingSystem.BuildingData;
 using FortressForge.BuildingSystem.Weapons;
@@ -18,9 +16,11 @@ namespace Tests.Weapon
 {
     public class WeaponInputHandlerRotationTests : InputTestFixture
     {
+        private const string WEAPON_TESTS = "WeaponTests";
         private GameObject _weaponInstance;
         private WeaponInputHandler _weaponInputHandler;
         private Transform _towerBase;
+        private Transform _cannonShaft;
         private WeaponBuildingTemplate _testConstants;
         private Keyboard _keyboard;
         private Mouse _mouse;
@@ -28,7 +28,6 @@ namespace Tests.Weapon
         private float
             TestsDelayTime =
                 0f; // Delay time for the tests to wait for the camera to move (Change this for optical debuging to 0.5f)
-
 
         /// <summary>
         /// Registers the keyboard input device and prepares the input system for test execution.
@@ -38,11 +37,12 @@ namespace Tests.Weapon
         public override void Setup()
         {
             base.Setup();
-
-            // Register and add keyboard and a mouse device for input simulation
             InputSystem.RegisterLayout<Keyboard>();
-            _keyboard = InputSystem.AddDevice<Keyboard>();
-            Assert.NotNull(_keyboard, "Keyboard device not found.");
+            InputSystem.AddDevice<Keyboard>();
+            Assert.That(InputSystem.devices, Has.Exactly(1).TypeOf<Keyboard>());
+            _keyboard = InputSystem.GetDevice<Keyboard>();
+            Assert.NotNull(_keyboard, "Error: Keyboard device not found.");
+
             InputSystem.AddDevice<Mouse>();
             _mouse = InputSystem.GetDevice<Mouse>();
             Assert.NotNull(_mouse, "Error: Mouse device not found.");
@@ -54,67 +54,42 @@ namespace Tests.Weapon
         /// </summary>
         private IEnumerator SetupSceneAndWeapon()
         {
-            // Load empty test scene
-            SceneManager.LoadScene("EmptyScene", LoadSceneMode.Single);
-            yield return new WaitUntil(() => SceneManager.GetActiveScene().name == "EmptyScene");
+            // Load test scene
+            SceneManager.LoadScene(WEAPON_TESTS);
+            yield return new WaitUntil(
+                () => SceneManager.GetActiveScene().name == WEAPON_TESTS,
+                new TimeSpan(0, 0, 10),
+                () => Assert.AreEqual(WEAPON_TESTS, SceneManager.GetActiveScene().name,
+                    "Failed to load GameOverlay scene within the timeout period.")
+            );
 
-            // 1. Load the Ammunition prefab
-            GameObject ammoPrefab = Resources.Load<GameObject>("Prefabs/Ammunition/CannonBall");
-            Assert.IsNotNull(ammoPrefab, "Ammunition prefab could not be loaded. Make sure it exists in a Resources folder.");
-            // Instantiate NetworkManager prefab for FishNet networking setup
             var networkManagerPrefab = Resources.Load<GameObject>("Prefabs/NetworkManager");
-            Assert.IsNotNull(networkManagerPrefab, "NetworkManager prefab not found!");
-            var networkManagerInstance = Object.Instantiate(networkManagerPrefab);
-            networkManagerInstance.name = "TestNetworkManager";
+            if (networkManagerPrefab != null)
+            {
+                var networkManagerInstance = Object.Instantiate(networkManagerPrefab);
+                networkManagerInstance.name = "TestNetworkManager";
+
+                yield return null;
+
+                if (!InstanceFinder.ServerManager.Started)
+                {
+                    InstanceFinder.ServerManager.StartConnection();
+                    yield return new WaitUntil(() => InstanceFinder.ServerManager.Started);
+                }
+
+                if (!InstanceFinder.ClientManager.Started)
+                {
+                    InstanceFinder.ClientManager.StartConnection();
+                    yield return new WaitUntil(() => InstanceFinder.ClientManager.Started);
+                }
+            }
+
+            SpawnWeaponBuildingPrefab();
             yield return null;
 
-            // Start server and client connections
-            InstanceFinder.ServerManager.StartConnection();
-            yield return new WaitUntil(() => InstanceFinder.ServerManager.Started);
-
-            InstanceFinder.ClientManager.StartConnection();
-            yield return new WaitUntil(() => InstanceFinder.ClientManager.Started);
-
-            // Instantiate weapon prefab and spawn it on server
-            var weaponPrefab = Resources.Load<GameObject>("Prefabs/Tier_1/Tier_1_Konzept");
-            Assert.IsNotNull(weaponPrefab, "Weapon prefab not found!");
-            _weaponInstance = Object.Instantiate(weaponPrefab);
-            yield return null;
-
-            var networkObject = _weaponInstance.GetComponent<NetworkObject>();
-            Assert.IsNotNull(networkObject, "Weapon prefab missing NetworkObject!");
-            InstanceFinder.ServerManager.Spawn(networkObject);
-            yield return null;
-
-            _weaponInputHandler = _weaponInstance.GetComponentInChildren<WeaponInputHandler>();
-            Assert.IsNotNull(_weaponInputHandler, "WeaponInputHandler not found!");
-
-            _testConstants = GetWeaponConstantsFromPrefabInstance(_weaponInputHandler);
-            Assert.IsNotNull(_testConstants, "WeaponBuildingTemplate reference on prefab is null.");
-
-            _towerBase = _weaponInstance.transform.Find("Geschuetzturm");
-            Assert.IsNotNull(_towerBase, "'Geschuetzturm' transform not found!");
+            _weaponInputHandler.GetComponent<PlayerInput>()?.ActivateInput();
         }
-
-        private WeaponBuildingTemplate GetWeaponConstantsFromPrefabInstance(WeaponInputHandler handler)
-        {
-            // Use reflection to get the private field _constants from WeaponInputHandler
-            var constantsField = typeof(WeaponInputHandler)
-                .GetField("_constants", BindingFlags.NonPublic | BindingFlags.Instance);
-
-            if (constantsField == null)
-                throw new Exception("Field '_constants' not found in WeaponInputHandler.");
-
-            var constants = (WeaponBuildingTemplate)constantsField.GetValue(handler);
-            return constants;
-        }
-
-        /// <summary>
-        /// Normalizes a given angle to be within the range [0, 360).
-        /// This is useful for comparing angles regardless of how Unity internally stores them.
-        /// </summary>
-        private float NormalizeRotation(float angle) => (angle + 360f) % 360f;
-
+        
         /// <summary>
         /// Simulates pressing the 'L' key to rotate the weapon turret to the right (clockwise).
         /// Asserts that the turret rotated approximately by the expected degrees based on rotation speed and delta time.
@@ -150,7 +125,7 @@ namespace Tests.Weapon
             //Setup
             yield return SetupSceneAndWeapon();
             _towerBase.localEulerAngles = Vector3.zero;
-            _weaponInputHandler.SendMessage("OnMouseDown"); // Enter fight mode to enable input actions
+            _weaponInputHandler.SendMessage("OnMouseDown");
 
             // rotate left
             Press(_keyboard.jKey);
@@ -169,9 +144,8 @@ namespace Tests.Weapon
         public IEnumerator AdjustAngle_IKeyPressed_StopsAtMaxAngle()
         {
             yield return SetupSceneAndWeapon();
-            var cannonShaft = _weaponInstance.transform.Find("Geschuetzturm/Lauf");
-            Assert.IsNotNull(cannonShaft, "Cannon shaft transform not found!");
-            cannonShaft.localEulerAngles =
+
+            _cannonShaft.localEulerAngles =
                 new Vector3(_testConstants.minCannonAngle, 0, 0); // Start at maximum pitch angle
             _weaponInputHandler.SendMessage("OnMouseDown"); // Enter fight mode to enable input actions
             yield return null;
@@ -182,7 +156,7 @@ namespace Tests.Weapon
             Release(_keyboard.iKey);
             yield return null;
 
-            float pitch = cannonShaft.localRotation.eulerAngles.x;
+            float pitch = _cannonShaft.localRotation.eulerAngles.x;
             if (pitch > 180f) pitch -= 360f;
 
             Assert.That(pitch, Is.LessThanOrEqualTo(_testConstants.maxCannonAngle + 0.1f),
@@ -193,9 +167,8 @@ namespace Tests.Weapon
         public IEnumerator AdjustAngle_KKeyPressed_StopsAtMinAngle()
         {
             yield return SetupSceneAndWeapon();
-            var cannonShaft = _weaponInstance.transform.Find("Geschuetzturm/Lauf");
-            Assert.IsNotNull(cannonShaft, "Cannon shaft transform not found!");
-            cannonShaft.localEulerAngles =
+
+            _cannonShaft.localEulerAngles =
                 new Vector3(_testConstants.maxCannonAngle, 0, 0); // Start at maximum pitch angle
             _weaponInputHandler.SendMessage("OnMouseDown"); // Enter fight mode to enable input actions
 
@@ -205,7 +178,7 @@ namespace Tests.Weapon
             Release(_keyboard.kKey);
             yield return null;
 
-            float pitch = cannonShaft.localRotation.eulerAngles.x;
+            float pitch = _cannonShaft.localRotation.eulerAngles.x;
             if (pitch > 180f) pitch -= 360f;
 
             Assert.That(pitch, Is.GreaterThanOrEqualTo(_testConstants.minCannonAngle - 0.1f),
@@ -213,7 +186,7 @@ namespace Tests.Weapon
         }
 
         [UnityTest]
-        public IEnumerator FireWeapon_SpaceKeyPressed_FiresOnce()
+        public IEnumerator FireWeapon_FiresOnce()
         {
             yield return SetupSceneAndWeapon();
 
@@ -228,15 +201,15 @@ namespace Tests.Weapon
 
             // Press and release space to fire
             Press(_keyboard.spaceKey);
-            yield return null; 
+            yield return null;
             Release(_keyboard.spaceKey);
-            
+
             // rotate left to stop the Autofiring
             Press(_keyboard.jKey);
             yield return new WaitForSeconds(TestsDelayTime);
             Release(_keyboard.jKey);
             yield return null;
-            
+
             // Wait until ammo has changed or timeout
             yield return new WaitUntil(() => _weaponInputHandler.GetCurrentAmmo() < initialAmmo);
 
@@ -245,6 +218,59 @@ namespace Tests.Weapon
             Assert.AreEqual(initialAmmo - 1, ammoAfterFire,
                 $"Expected ammo to decrease by 1 after firing. Actual: {ammoAfterFire}");
         }
+
+        private void SpawnWeaponBuildingPrefab()
+        {
+            // Load and instantiate prefab
+            GameObject weaponPrefab = Resources.Load<GameObject>("Prefabs/Tier_1/Tier_1_Konzept");
+            Assert.IsNotNull(weaponPrefab, "Weapon prefab not found in Resources.");
+
+            _weaponInstance = Object.Instantiate(weaponPrefab);
+            Assert.IsNotNull(_weaponInstance, "Weapon prefab instance could not be created.");
+
+            // Ensure ammunition is present
+            GameObject ammoPrefab = Resources.Load<GameObject>("Prefabs/Ammunition/CannonBall");
+            Assert.IsNotNull(ammoPrefab,
+                "Ammunition prefab could not be loaded. Make sure it exists in a Resources folder.");
+
+            // Get and check WeaponInputHandler
+            _weaponInputHandler = _weaponInstance.GetComponent<WeaponInputHandler>();
+            Assert.IsNotNull(_weaponInputHandler, "WeaponInputHandler component not found on the weapon prefab.");
+
+            // Optional: If it expects registration, but isn't required for test logic, skip or mock
+            WeaponBuildingManager.Instance?.RegisterWeaponBuilding(_weaponInputHandler);
+
+            // Get template constants using reflection
+            _testConstants = GetWeaponConstantsFromPrefabInstance(_weaponInputHandler);
+            Assert.IsNotNull(_testConstants, "WeaponBuildingTemplate reference on prefab is null.");
+
+            // Get transforms for turret base and cannon shaft
+            _towerBase = _weaponInstance.transform.Find("Geschuetzturm");
+            Assert.IsNotNull(_towerBase, "'Geschuetzturm' transform not found!");
+
+            _cannonShaft = _weaponInstance.transform.Find("Geschuetzturm/Lauf");
+            Assert.IsNotNull(_cannonShaft, "Cannon shaft transform not found!");
+        }
+
+
+        private WeaponBuildingTemplate GetWeaponConstantsFromPrefabInstance(WeaponInputHandler handler)
+        {
+            // Use reflection to get the private field _constants from WeaponInputHandler
+            var constantsField = typeof(WeaponInputHandler)
+                .GetField("_constants", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (constantsField == null)
+                throw new Exception("Field '_constants' not found in WeaponInputHandler.");
+
+            var constants = (WeaponBuildingTemplate)constantsField.GetValue(handler);
+            return constants;
+        }
+
+        /// <summary>
+        /// Normalizes a given angle to be within the range [0, 360).
+        /// This is useful for comparing angles regardless of how Unity internally stores them.
+        /// </summary>
+        private float NormalizeRotation(float angle) => (angle + 360f) % 360f;
 
         /// <summary>
         /// Cleans up the weapon instance after each test to avoid memory leaks or leftover state.
